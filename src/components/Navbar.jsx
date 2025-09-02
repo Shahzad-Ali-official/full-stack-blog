b79321c1b709c1cffb0a984617dfa4190657746a
Shahzad <shahzadofficialfirms@gmail.comgit config user.namegit config --global user.email shahzadofficialfirms@gmail.com>
2025-09-03 03:24:36 +0500

    SEP 3, 2025

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Image , ImageKitProvider } from '@imagekit/react';
 
 import { useState } from "react";
+import { Link } from 'react-router';
 
 
 
@@ -11,7 +12,7 @@ const Navbar = () => {
 
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
         {/* LOGO */}
-        <div className="flex items-center gap-4 text-2xl font-bold">
+        <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
             <ImageKitProvider
             publicKey={import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY}
             urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT}
@@ -28,7 +29,7 @@ const Navbar = () => {
             />
             </ImageKitProvider>
             <span>Ai blogs</span>
-        </div>
+        </Link>
         {/* MOBILE MENU */}
         <div className="md:hidden">
            {/* HAMBURGER ICON */}
@@ -43,11 +44,13 @@ const Navbar = () => {
              ${isopen ? "right-0" : "-right-[100%]"}`}
              >
               {/* You can map over your menu items here */}
-              <a href="/about" className=" text-2xl p-4">About</a>
-              <a href="/contact" className=" text-2xl p-4">Contact</a>
-              <a href="/trending" className=" text-2xl p-4">Trending🎉</a>
-              <a href="/login">
-              </a>
+              <Link to= "/" className=" text-2xl p-4">Home</Link>
+              <Link to= "/about" className=" text-2xl p-4">About</Link>
+              <Link to= "/contact" className=" text-2xl p-4">Contact</Link>
+              <Link to= "/trending" className=" text-2xl p-4">Trending🎉</Link>
+              
+              <Link to= "/login"></Link>
+              
               <button className="py-2 px-4 rounded-3xl bg-blue-800 text-2xl text-white" >login👋</button>
               
             </div>
@@ -55,15 +58,14 @@ const Navbar = () => {
         {/* DESKTOP MENU */}
             <div className="hidden md:flex gap-4 items-center">
         
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-            <a href="/trending">Trending🎉</a>
-            
-              <a href="/login">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/trending">Trending🎉</Link>
+               <Link to="/login"></Link>
+                 <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white" >login👋</button>
+
               
-              </a>
-              <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white" >login👋</button>
             </div>  
         </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>
 
 )
